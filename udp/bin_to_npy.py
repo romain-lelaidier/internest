2f9c9a29e14acc7864d71b360bbd5560c6243bf7
Noé Daniel <44977477+PyNoe@users.noreply.github.com>
2026-01-28 14:31:51 +0100

    Implement to_human_readable function
    Added a function to convert binary data to a human-readable format.

--- udp/bin_to_npy.py
+++ udp/bin_to_npy.py
@@ -31,13 +31,26 @@ def date_data_tab(file_path, verbose=False):
 
     return dates, np.array(data)
 
+def to_human_readable(dates, data, verbose=False):
+    dates = [time_decryption(date) for date in dates]
+    data = [
+        [point_decryption(data[i, j]) for j in range(len(data[i]))]
+        for i in range(len(data))
+    ]
+    data = np.array(data)
+
+    if verbose:
+        print("len(dates) : ", len(dates), "; dates[:5] : ", dates[:5])
+        print("len(data) : ", len(data), "; data[0, :5] : ", data[0, :5])
+
+    return dates, data
 
 """ Pipeline """
 
 # 1. Lire le binaire brut
 bin_dates, bin_data = date_data_tab("enregistrement.bin")
 
-# 2. Convertir en valeurs lisibles (votre fonction existante)
+# 2. Convertir en valeurs lisibles
 # Cela sépare clairement le temps et l'amplitude
 dates_reelles, signal_audio = to_human_readable(bin_dates, bin_data)
 
