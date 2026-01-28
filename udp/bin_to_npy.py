5694358d35ab77b42128a7dad2602a7f71cf7a97
Noé Daniel <44977477+PyNoe@users.noreply.github.com>
2026-01-28 14:31:24 +0100

    Implement binary data reading and conversion function
    Added a function to read binary data and convert it into a format readable by Python, including optional verbose output.

--- udp/bin_to_npy.py
+++ udp/bin_to_npy.py
@@ -6,6 +6,34 @@ Objectif :
 Encoder les .bin dans un fichier lisible par Python pour le traitement du signal
 """
 
+
+"""Fonctions reprises du script de Colin"""
+
+def date_data_tab(file_path, verbose=False):
+    with open(file_path, "rb") as fb:
+        file = fb.read()
+        dates = [
+            file[CHUNK_SIZE // 8 * i : CHUNK_SIZE // 8 * i + 8]
+            for i in range(CHUNK_PER_FILE)
+        ]
+        data = [
+            file[CHUNK_SIZE // 8 * i + 8 : CHUNK_SIZE // 8 * (i + 1)]
+            for i in range(CHUNK_PER_FILE)
+        ]
+    data = [
+        [
+            data[i][j : j + 1] for j in range(len(data[i]))
+        ]  # data[i][j] converts the signed binary to int as if it were unsigned
+        for i in range(len(data))
+    ]
+    if verbose:
+        verbose_data_bin_tab(file, dates, data)
+
+    return dates, np.array(data)
+
+
+""" Pipeline """
+
 # 1. Lire le binaire brut
 bin_dates, bin_data = date_data_tab("enregistrement.bin")
 
