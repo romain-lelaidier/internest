5c47b1445e44d316d7304fe4438c7cc4ecfff6b0
romain-lelaidier <romain.lelaidier@etu.minesparis.psl.eu>
2026-02-10 15:19:28 +0100

    Minor bugs

--- server/localisation2.py
+++ server/localisation2.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import signal, ndimage
 from scipy.optimize import least_squares
 from sklearn.cluster import DBSCAN
+from scipy.io.wavfile import write
 
 from utils import micros
 from config import CONFIG
@@ -42,9 +43,11 @@ def merge_boxes(boxes):
 
 def aggregate_boxes(boxes):
     all_boxes = []
-    for id, bxs in boxes.items():
+    for mac, bxs in boxes.items():
         all_boxes.extend(bxs)  # boxes_i is the list of boxes for group i
     n_boxes = len(all_boxes)
+    if n_boxes == 0:
+        return []
     overlap_matrix = np.zeros((n_boxes, n_boxes))
     for i in range(n_boxes):
         for j in range(n_boxes):
@@ -58,10 +61,10 @@ def aggregate_boxes(boxes):
             if label not in clusters:
                 clusters[label] = []
             clusters[label].append(all_boxes[i])
-    return [merge_boxes(boxes) for boxes in clusters.values()]
+    return [ merge_boxes(boxes) for boxes in clusters.values() ]
 
-def extract_patch(samples, id, box):
-    sample = samples[id]
+def extract_patch(samples, mac, box):
+    sample = samples[mac]
     tmin, tmax, fmin, fmax = box
     imin = int(tmin * CONFIG.SAMPLE_RATE)
     imax = int(tmax * CONFIG.SAMPLE_RATE)
@@ -91,9 +94,9 @@ def gcc_phat(sig, refsig, fs=1, max_tau=None, interp=16):
     tau = shift / float(interp * fs)
     return tau, cc
 
-def box_tdoa(samples, box, id1, id2):
-    s1 = extract_patch(samples, id1, box)
-    s2 = extract_patch(samples, id2, box)
+def box_tdoa(samples, box, mac1, mac2):
+    s1 = extract_patch(samples, mac1, box)
+    s2 = extract_patch(samples, mac2, box)
     return gcc_phat(s2, s1, fs=CONFIG.SAMPLE_RATE)
 
 def multilateration_residual(source, mic_positions, distance_differences):
@@ -103,6 +106,8 @@ def multilateration_residual(source, mic_positions, distance_differences):
         d_j = np.linalg.norm(source - mic_positions[j])
         residuals.append(d_i - d_j + diff)
     return np.array(residuals)
+
+nth = 0
 
 def localiser(esps, t1, t2):
     samples = {}
@@ -120,16 +125,17 @@ def localiser(esps, t1, t2):
 
         # threshold filtering
         i = Sxx > Sxx.max() / 20                                # keeping only high-energy values
+        i = i & np.repeat([f > 800], len(t), axis=0).T          # keeping only frequencies above 800 Hz
         # building boxes
         ii = expand_and_filter_true_regions(i, min_size=20)     # aggregating those values and removing the only ones
         labeled_array, num_features = ndimage.label(ii)
-        boxes[id] = []
+        boxes[mac] = []
         for i in range(num_features):
             iii = labeled_array == i+1
             # retrieving box boundaries
             true_indices = np.where(iii)
             x_min, x_max, y_min, y_max = np.min(true_indices[1]), np.max(true_indices[1]), np.min(true_indices[0]), np.max(true_indices[0])
-            boxes[id].append((t[x_min], t[x_max], f[y_min], f[y_max]))
+            boxes[mac].append((t[x_min], t[x_max], f[y_min], f[y_max]))
 
     # aggregating boxes from all spectrograms together
     agg_boxes = aggregate_boxes(boxes)
@@ -142,14 +148,21 @@ def localiser(esps, t1, t2):
     sound_guesses = []
     for box in agg_boxes:
         tdoas = []
-        for i, (idi, si) in enumerate(samples.items()):
-            for j, (idj, sj) in enumerate(samples.items()):
+        sie_max = 0
+        sie_max_si = None
+        for i, (maci, si) in enumerate(samples.items()):
+            for j, (macj, sj) in enumerate(samples.items()):
                 if i >= j: continue
                 # computing tdoa
-                tdoa, cc = box_tdoa(samples, box, idi, idj)
+                tdoa, cc = box_tdoa(samples, box, maci, macj)
                 # removing values that are incoherent with the geometry
-                if np.abs(tdoa) > 1.2 * np.linalg.norm(esps[idi].position - esps[idj].positions): continue
-                tdoas.append((idi, idj, tdoa))
+                if np.abs(tdoa) > 1.2 * np.linalg.norm(esps[maci].position - esps[macj].position): continue
+                tdoas.append((maci, macj, tdoa))
+            
+            sie = np.trapz(np.abs(si)**2, t) / len(si)
+            if sie > sie_max:
+                sie_max = sie
+                sie_max_si = si
 
         distance_differences = [(i, j, tdoa * 343) for i, j, tdoa in tdoas]
         result = least_squares(
@@ -158,7 +171,10 @@ def localiser(esps, t1, t2):
             args=(positions, distance_differences),
         )
         estimated_sound_origin = result.x
-        sound_guesses.append(estimated_sound_origin, box)
+        print(f"saving {nth}")
+        write(f"{nth}.wav", CONFIG.SAMPLE_RATE, np.int16(sie_max_si / np.max(np.abs(sie_max_si)) * 32767))
+        nth += 1
+        sound_guesses.append((estimated_sound_origin, box))
 
     print("GUESSES:", sound_guesses)
 
@@ -166,6 +182,8 @@ def routine_localiser(esps):
     while True:
         t = micros()
         target_t2 = t - CONFIG.BUFFER_DELAY_US
-        target_t1 = target_t2 - CONFIG.WINDOW_SIZE_VAD_US        
-        localiser(esps, target_t1, target_t2)  
-        time.sleep(CONFIG.COMPUTE_INTERVAL_US / 1e6)
+        target_t1 = target_t2 - CONFIG.WINDOW_SIZE_VAD_US
+        try:
+            localiser(esps, target_t1, target_t2)
+        finally:
+            time.sleep(CONFIG.COMPUTE_INTERVAL_US / 1e6)
