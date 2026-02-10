76c26b98fbcd37254355e4bac7904007bd52a514
romain-lelaidier <romain.lelaidier@etu.minesparis.psl.eu>
2026-02-10 17:57:00 +0100

    ESP

--- server/esp.py
+++ server/esp.py
@@ -1,7 +1,11 @@
 import numpy as np
 from scipy import signal
+import threading
 
 from config import CONFIG
+from birdnet_loop import _esp_loop
+from utils import micros
+from scipy.io.wavfile import write
 
 WINDOW_BUFFER_SIZE = CONFIG.MAX_WINDOW_S * CONFIG.SAMPLE_RATE
 
@@ -30,6 +34,21 @@ class ESP:
     def index_to_time(self, i):
         return self.t0 + i * 1e6 / CONFIG.SAMPLE_RATE
     
+    def register_buzztime(self, te, f):
+        self.buzzobjs.append({
+            'te': te,
+            'f': f
+        })
+        print(self.buzzobjs)
+    
+    def try_assign_buzz(self, samples, t1, t2):
+        return
+        f, i = look_for_buzzes(samples, self.frequency)
+        if i > 0:
+            for buzzobj in self.buzzobjs:
+                if np.abs(buzzobj['f'] - f) < 5:
+                    buzzobj['t'] = t1 + (t2 - t1) * i / len(samples)
+
     def receive_packet(self, t2, samples):
         # un paquet vient d'être reçu par le serveur UDP.
         # cette fonction l'interpole pour prendre en compte la fréquence d'échantillonnage effective.
@@ -70,6 +89,8 @@ class ESP:
         self.last_written_index = write_at + len(interpolated_samples)
         self.buffer_end_t = t2
 
+        # write('test.wav', CONFIG.SAMPLE_RATE, np.int16(self.buffer / np.max(np.abs(self.buffer)) * 32767))
+
     def read_window(self, t1, t2):
         # retourne une fenêtre audio donnant l'intersection maximale entre t1 et t2
         # renvoie t1_reel, t2_reel, samples
@@ -92,3 +113,13 @@ class ESP:
             return int(t_r1), int(t_r2), self.buffer[i_r1 % WINDOW_BUFFER_SIZE : i_r2 % WINDOW_BUFFER_SIZE]
         
         return int(t_r1), int(t_r2), np.concatenate((self.buffer[i_r1 % WINDOW_BUFFER_SIZE : WINDOW_BUFFER_SIZE], self.buffer[0 : i_r2 % WINDOW_BUFFER_SIZE]))
+
+    # def t_to_te(self, t):
+    #     if self.synced:
+    #         return None
+    #     return t
+
+    # def read_window(self, t1, t2):
+    #     if self.synced:
+    #         return self.read_window_esp(self.t_to_te(t1), self.t_to_te(t2))
+    #     return -1, -1, np.array([], dtype=np.int16)
